700177c5c0a18ddbc84f3e4a3515024db69faadb
郑宁 <zhengning@badambiz.com>
2018-03-23 19:05:39 +0800

    增加nodejs demo

---
+++ nodejs/login_demo/login_nodejs_demo.js
@@ -0,0 +1,27 @@
+var http = require('http');
+var querystring = require('querystring');
+ 
+var contents = querystring.stringify({
+    appid:'', //此处填写游戏APPID
+    token:''  //此处填写token
+});
+ 
+var options = {
+    host:'test.open.badambiz.com',
+    path:'/api/user/verify_token/',
+    method:'POST',
+    headers:{
+        'Content-Type':'application/x-www-form-urlencoded',
+        'Content-Length':contents.length
+    }
+}
+ 
+var req = http.request(options, function(res){
+    res.setEncoding('utf8');
+    res.on('data',function(data){
+        console.log("data:",data);
+    });
+});
+ 
+req.write(contents);
+req.end;
